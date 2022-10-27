963aeaebe0c317f39a2e02c24d688d50a1a3a597
Cécilia Ruin <cecilia.ruin@gmail.com>
2022-10-27 14:56:41 +0200

    change data

--- model/categories.js
+++ model/categories.js
@@ -1,12 +1,60 @@
 const categories = [
-    {id: 1, name: 'Indoor', plants : [ { id: 1, name: 'Plante1', price: 100, description: 'lalalalalla', image: "/images/products/plante1.png"  },
-    { id: 2, name: 'Plante2', price: 200, description: 'lalalalalla2', image: "/images/products/plante2.png"  },
-    { id: 3, name: 'Plante3', price: 300, description: 'lalalalalla3', image: "/images/products/plante3.png"  },
-    { id: 4, name: 'Plante4', price: 400, description: 'lalalalalla4', image: "/images/products/plante4.png"  },
-    { id: 5, name: 'Plante5', price: 500, description: 'lalalalalla5', image: "/images/products/plante5.png"  },
-    { id: 6, name: 'Plante6', price: 600, description: 'lalalalalla6', image: "/images/products/plante6.png"  }]},
-    {id: 2, name: 'Outdoor', plants : [{ id: 7, name: 'Plante7', price: 700, description: 'lalalalalla7', image: "/images/products/plante7.png"  },
-    { id: 8, name: 'Plante8', price: 800, description: 'lalalalalla8', image: "/images/products/plante8.png"  }
+    {id: 1, name: 'Indoor', plants : [ {
+        "id": 1,
+        "name": "Trois plantes",
+        "price": 100,
+        "description": "Trois plante dans trois pots moderne pour votre intérieur",
+        "image": "/images/products/plante1.png"
+      },
+      {
+        "id": 2,
+        "name": "Ficulus",
+        "price": 200,
+        "description": "Ficulus dans un pot moderne pour votre intérieur",
+        "image": "/images/products/plante2.png"
+      },
+      {
+        "id": 3,
+        "name": "Grendtije",
+        "price": 300,
+        "description": "Grendtije dans un pot moderne pour votre intérieur",
+        "image": "/images/products/plante3.png"
+      },
+      {
+        "id": 4,
+        "name": "Grendefeuyu",
+        "price": 400,
+        "description": "Grendefeuyu dans un pot moderne pour votre intérieur",
+        "image": "/images/products/plante4.png"
+      },
+      {
+        "id": 5,
+        "name": "Ficusempra",
+        "price": 500,
+        "description": "Ficusempra dans un pot moderne pour votre intérieur",
+        "image": "/images/products/plante5.png"
+      },
+      {
+        "id": 6,
+        "name": "Grendabre",
+        "price": 600,
+        "description": "Grendabre dans un pot moderne pour votre intérieur",
+        "image": "/images/products/plante6.png"
+      },]},
+    {id: 2, name: 'Outdoor', plants : [{
+        "id": 7,
+        "name": "Feuillimi",
+        "price": 700,
+        "description": "Feuillimi pour votre extérieur",
+        "image": "/images/products/plante7.png"
+      },
+      {
+        "id": 8,
+        "name": "Saladus",
+        "price": 800,
+        "description": "Saladus pour votre extérieur",
+        "image": "/images/products/plante8.png"
+      }
 ]}
 ]
 module.exports = categories
